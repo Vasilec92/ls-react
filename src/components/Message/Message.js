5c944060abec4c65ceeb3dc4ddcd79e67dd24cde
Anna Vasileva <vasilec1992@yandex.rum>
2022-05-02 11:17:33 +0200

    dz1

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,6 +1,10 @@
 import React from "react";
 import styles from "./message.module.css";
 
-export const Message = ({ msg }) => {
-  return <div className={styles.msg}>{msg}</div>;
+export const Message = ({ msg, num }) => {
+  return (
+    <div className={styles.msg}>
+      {msg} &&& {num}
+    </div>
+  );
 };
